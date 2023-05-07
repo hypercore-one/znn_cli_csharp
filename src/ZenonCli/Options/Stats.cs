cae8eae06d7e75c74942349dbedacff68a1e9acb
KingGorrin <YankeeDoodlePigeon@protonmail.com>
2023-05-07 13:12:21 +0200

    Add stats commands

---
+++ src/ZenonCli/Options/Stats.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+
+namespace ZenonCli.Options
+{
+    public class Stats
+    {
+        [Verb("os.info", HelpText = "Get the os info")]
+        public class OsInfo : ConnectionOptions
+        { }
+
+        [Verb("process.info", HelpText = "Get the process info")]
+        public class ProcessInfo : ConnectionOptions
+        { }
+
+        [Verb("network.info", HelpText = "Get the network info")]
+        public class NetworkInfo : ConnectionOptions
+        { }
+
+        [Verb("sync.info", HelpText = "Get the sync info")]
+        public class SyncInfo : ConnectionOptions
+        { }
+    }
+}
